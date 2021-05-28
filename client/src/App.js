d964d42809b4d96538c3621540441ec3f6859a19
Emmanuel Flambard <emmanuel.flambard@gmail.com>
2021-05-28 01:13:31 -0400

    NavBar beta version/mobile hamburger menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import GlobalStyles from "./components/GlobalStyles"
+import React from "react";
+import GlobalStyles from "./components/GlobalStyles";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import LandingPage from "./components/LandingPage"
+import LandingPage from "./components/LandingPage";
+import Navbar from "./components/Navbar/Navbar";
+
 ///
 function App() {
-
   return (
     <BrowserRouter>
-          <GlobalStyles />
-          <Switch>
-          <Route exact path="/">
+      <GlobalStyles />
+      <Switch>
+        <Route exact path="/">
+          <div className="App">
+            <Navbar />
+          </div>
           <LandingPage />
         </Route>
-        </Switch>
+      </Switch>
     </BrowserRouter>
-
-  )
+  );
 }
 
 export default App;
